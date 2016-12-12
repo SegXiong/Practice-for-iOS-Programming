bf770855a332f7e8d4edbd351bdaea080219cd06
SegXiong <Moe@MoredeMacBook-Pro.local>
2016-12-12 17:25:18 +0800

    Chapter 7
    添加捏合缩放功能

---
+++ 第七章/Hypnosister/Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by More Xiong on 2016/11/3.
+//  Copyright © 2016年 More Xiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIScrollViewDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
